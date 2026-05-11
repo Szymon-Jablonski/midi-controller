5741d8bef948a8bfa1621b020c09a1cea097723e
Szymon Jablonski <jablonskiszymon04@gmail.com>
2026-05-11 11:00:23 +0200

    fix: Removed artifacts.

--- Debug/Core/Src/tinyusb/host/usbh.d
+++
@@ -1,9 +0,0 @@
-Core/Src/tinyusb/host/usbh.o: ../Core/Src/tinyusb/host/usbh.c \
- /home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/tusb_option.h \
- /home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_compiler.h \
- ../Core/Inc/tusb_config.h \
- /home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_mcu.h
-/home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/tusb_option.h:
-/home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_compiler.h:
-../Core/Inc/tusb_config.h:
-/home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_mcu.h:
--- Debug/Core/Src/tinyusb/typec/usbc.d
+++
@@ -1,9 +0,0 @@
-Core/Src/tinyusb/typec/usbc.o: ../Core/Src/tinyusb/typec/usbc.c \
- /home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/tusb_option.h \
- /home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_compiler.h \
- ../Core/Inc/tusb_config.h \
- /home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_mcu.h
-/home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/tusb_option.h:
-/home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_compiler.h:
-../Core/Inc/tusb_config.h:
-/home/sjablons/EAC/embedded/project/midi-controller/Core/Src/tinyusb/common/tusb_mcu.h:
